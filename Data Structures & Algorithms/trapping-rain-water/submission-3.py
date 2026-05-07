d948b0bc630260a66968fa0c0742b3518eef93a6
Duc Truong <44458888+ducquangtruong@users.noreply.github.com>
2026-05-07 12:26:13 +0800

    Add: trapping-rain-water - submission-3

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-3.py
@@ -0,0 +1,17 @@
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        # At any point, keep track of the highest bar at its left and right
+        maxLeft, maxRight = height[0], height[-1]
+        res = 0
+        l, r = 0, len(height) -1
+        while l < r:
+            if maxLeft < maxRight:
+                l += 1
+                maxLeft = max(maxLeft, height[l])
+                res += maxLeft - height[l]
+            else:
+                r -= 1
+                maxRight = max(maxRight, height[r])
+                res += maxRight - height[r]
+        
+        return res
