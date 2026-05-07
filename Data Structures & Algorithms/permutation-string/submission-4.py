1eb237aa23f792e417a0a8d41625eeefa4cd6363
Duc Truong <44458888+ducquangtruong@users.noreply.github.com>
2026-05-07 13:12:15 +0800

    Add: permutation-string - submission-4

---
+++ Data Structures & Algorithms/permutation-string/submission-4.py
@@ -0,0 +1,21 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        # Frequency array for s1
+        # Array instead of dict because only 26 letters
+        cnt1 = [0] * 26
+        for chr in s1:
+            cnt1[ord(chr) - ord('a')] += 1
+
+        cnt2 = [0] * 26
+        start, end = 0, 0
+
+        while end < len(s2):
+            if end - start == len(s1):
+                if cnt1 == cnt2:
+                    return True
+                cnt2[ord(s2[start]) - ord('a')] -= 1
+                start += 1
+            cnt2[ord(s2[end]) - ord('a')] += 1
+            end += 1
+        
+        return cnt1 == cnt2
